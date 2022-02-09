29e34b95e7556d18da45dafb7c8ac79e7cd9f9ee
EndProgramm <eg.network.de@gmail.com>
2022-02-09 11:11:20 +0100

    Suchfunktionen im Model entfernen der Fehlplatzierten Datenbank

--- Model/main.py
+++ Model/main.py
@@ -5,19 +5,33 @@ import sqlite3 as sqlite
 
 
 class Model:
+    def __init__(self, db_name="Inventar.db"):
+        self.db = db_name
+
     def suchanfrage_GLEICH(self, tabelle, spalte, wert):
         con = sqlite.connect(self.db)
         cur = con.cursor()
+
         sql = f"SELECT {spalte} FROM {tabelle} WHERE {spalte} = {wert}"
-    
+        cur.execute(sql)
+
+        return cur.fetchall()
+
+    def suchanfrage_LIKE(self, tabelle, spalte, wert):
+        con = sqlite.connect(self.db)
+        cur = con.cursor()
+
+        sql = f"SELECT {spalte} FROM {tabelle} WHERE {spalte} LIKE {wert}"
+        cur.execute(sql)
+        return cur.fetchall()
+
     def datensatz_loeschen(self, mId):
-        connection = sql.connect("Inventar.db")
+        connection = sqlite.connect(self.db)
         cursor = connection.cursor()
-        sql="DELETE FROM Material WHERE id='"+mId+"';"
+        sql = "DELETE FROM Material WHERE id='"+mId+"';"
         cursor.execute(sql)
         connection.commit()
         connection.close()
-
 
 
 if __name__ == '__main__':
