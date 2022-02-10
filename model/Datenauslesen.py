9c17694e9704e147af7feffb14d57f56b5aadcad
Asuke03 <61423412+Asuke03@users.noreply.github.com>
2022-02-10 23:43:42 +0100

    Add files via upload

---
+++ model/Datenauslesen.py
@@ -0,0 +1,28 @@
+#Daten aus der Datenbank auskesen
+#Lisa-Marie Frauböse, Malte Reimer
+
+import os, sqlite3
+
+def sql(command):
+    # Herstellen der Verbindung mit der Datenbank
+    connection = sqlite3.connect("Inventar.db")
+    # Über den sogenannten Cursor können Anfragen an die Datenbank gestellt werden
+    cursor = connection.cursor()
+    # Tabellen erzeugen
+    sql = command
+    # Hier wird über den Cursor den Anfrage gestellt
+    cursor.execute(sql)
+    # Im ergebnis speichern wir das, was wir vom Curser zurück erhalten
+    ergebnis = cursor.fetchall()
+    # commit schließt die Transaktion ab und speichert die Änderung dauerhaft
+    connection.commit()
+    # Schließen der Datenbankverbindung
+    connection.close()
+    if ergebnis:
+        return ergebnis
+    else:
+        return None
+    
+def alleDaten():
+    return sql("SELECT * FROM Material")
+    
