188b89cdb7e8f96daef06520154a003b4c4a6a94
Hochspannungswerk <61975737+Hochspannungswerk@users.noreply.github.com>
2022-02-23 11:10:35 +0100

    updateInventory gemerged von Marcel

--- model/model.py
+++ model/model.py
@@ -30,6 +30,10 @@ class Model():
         sql = "DELETE FROM Material WHERE id='"+mId+"';"
         self.zeiger.execute(sql)
         self.verbindung.commit()
+        
+    def updateInventory(self,id, name,typ,kategorie,raum,ausgeliehen,status,anzahl,bemerkung): #Values as String with Value, Empty → None
+        self.zeiger.execute('UPDATE "Material" SET Name = ?, Typ = ?, Kategorie = ?, Raum = ?, Ausgeliehen = ?, Status = ?, Anzahl = ?, Bemerkung = ? WHERE MID = ?;', (name,typ,kategorie,raum,ausgeliehen,status,anzahl,bemerkung,id))
+        self.verbindung.commit()
 
 
 if __name__ == '__main__':
