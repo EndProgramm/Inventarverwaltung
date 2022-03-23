5b3a42c3e12414b78a9e756097c0502044e69720
EndProgramm <eg.network.de@gmail.com>
2022-03-23 10:18:23 +0100

    gemetzel aufgeräumt diesmal wirklich maximized starten neue Farbe
    Signed-off-by: EndProgramm <eg.network.de@gmail.com>

--- main.py
+++ main.py
@@ -5,24 +5,14 @@
 Beschreibung: \n
 Basis der UI, implementiert mit UIApp
 """
-import time
 
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.core.window import Window
-from kivy.app import App
-from kivy.uix.widget import Widget
 from kivy.uix.popup import Popup
-from kivy.lang.builder import Builder
-from kivy.uix.spinner import Spinner
 from kivy.uix.label import Label
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty, StringProperty, NumericProperty, ListProperty
+from kivy.properties import ObjectProperty
 from kivy.uix.screenmanager import Screen, ScreenManager
-from kivy.uix.recycleview import RecycleView
-from kivy.uix.recyclegridlayout import RecycleGridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.clock import Clock
 from kivy.uix.floatlayout import FloatLayout
 
 from controller.controller import Controller
@@ -46,51 +36,51 @@ class AddConsum(Screen):
 
 
 class Popups(FloatLayout):
-    popup_close = ObjectProperty(None)
+    popupClose = ObjectProperty(None)
     refreshTable = ObjectProperty(None)
 
-    typ_spinner = ObjectProperty()
-    kategorie_spinner = ObjectProperty()
-    raum_ent = ObjectProperty()
-    zustand_spinner = ObjectProperty()
-    anzahl_von_ent = ObjectProperty()
-    anzahl_bis_ent = ObjectProperty()
-    ausleihbar_spinner = ObjectProperty()
+    typSpinner = ObjectProperty()
+    kategorieSpinner = ObjectProperty()
+    raumEnt = ObjectProperty()
+    zustandSpinner = ObjectProperty()
+    anzahlVonEnt = ObjectProperty()
+    anzahlBisEnt = ObjectProperty()
+    ausleihbarSpinner = ObjectProperty()
 
     def kategorie(self):
         return control.getKategorie()
 
     def speichern(self):
         dict = {
-            "typ": self.typ_spinner.text,
-            "kategorie": self.kategorie_spinner.text,
-            "raum": self.raum_ent.text,
-            "zustand": self.zustand_spinner.text,
-            "anzahl_von": self.anzahl_von_ent.text,
-            "anzahl_bis": self.anzahl_bis_ent.text,
-            "ausleibahrkeit": self.ausleihbar_spinner.text
+            "typ": self.typSpinner.text,
+            "kategorie": self.kategorieSpinner.text,
+            "raum": self.raumEnt.text,
+            "zustand": self.zustandSpinner.text,
+            "anzahl_von": self.anzahlVonEnt.text,
+            "anzahl_bis": self.anzahlBisEnt.text,
+            "ausleibahrkeit": self.ausleihbarSpinner.text
         }
         return control.filterSpeichern(dict)
 
 
 class PopupAddGG(FloatLayout):  # Gebrauchsgegenstand
-    popup_add_GG_close = ObjectProperty(None)
-    name_ent = ObjectProperty()
-    kategorie_ent = ObjectProperty()
-    raum_ent = ObjectProperty()
-    zustand_spinner = ObjectProperty()
-    bemerkung_ent = ObjectProperty()
-    ausleihbar_checkbox = ObjectProperty()
+    popupAddGGClose = ObjectProperty(None)
+    nameEnt = ObjectProperty()
+    kategorieEnt = ObjectProperty()
+    raumEnt = ObjectProperty()
+    zustandSpinner = ObjectProperty()
+    bemerkungEnt = ObjectProperty()
+    ausleihbarCheckbox = ObjectProperty()
 
     def speichern(self):
         dict = {
-            "name": self.name_ent.text,
+            "name": self.nameEnt.text,
             "typ": "Gg",
-            "kategorie": self.kategorie_ent.text,
-            "raum": self.raum_ent.text,
-            "ausleibahrkeit": self.ausleihbar_checkbox.active,
-            "zustand": self.zustand_spinner.text,
-            "bemerkung": self.bemerkung_ent.text
+            "kategorie": self.kategorieEnt.text,
+            "raum": self.raumEnt.text,
+            "ausleibahrkeit": self.ausleihbarCheckbox.active,
+            "zustand": self.zustandSpinner.text,
+            "bemerkung": self.bemerkungEnt.text
         }
         control.saveObject(dict)
         c = control.saveObject(dict)
@@ -102,23 +92,23 @@ class PopupAddGG(FloatLayout):  # Gebrauchsgegenstand
 
 
 class PopupAddVG(FloatLayout):  # Verbrauchsgegenstand
-    popup_add_VG_close = ObjectProperty(None)
-    name_ent = ObjectProperty()
-    kategorie_ent = ObjectProperty()
-    raum_ent = ObjectProperty()
-    zustand_spinner = ObjectProperty()
-    bemerkung_ent = ObjectProperty()
-    anzahl_ent = ObjectProperty()
+    popupAddVGClose = ObjectProperty(None)
+    nameEnt = ObjectProperty()
+    kategorieEnt = ObjectProperty()
+    raumEnt = ObjectProperty()
+    zustandSpinner = ObjectProperty()
+    bemerkungEnt = ObjectProperty()
+    anzahlEnt = ObjectProperty()
 
     def speichern(self):
         dict = {
-            "name": self.name_ent.text,
+            "name": self.nameEnt.text,
             "typ": "Vg",
-            "kategorie": self.kategorie_ent.text,
-            "raum": self.raum_ent.text,
-            "anzahl": self.anzahl_ent.text,
-            "zustand": self.zustand_spinner.text,
-            "bemerkung": self.bemerkung_ent.text
+            "kategorie": self.kategorieEnt.text,
+            "raum": self.raumEnt.text,
+            "anzahl": self.anzahlEnt.text,
+            "zustand": self.zustandSpinner.text,
+            "bemerkung": self.bemerkungEnt.text
         }
         control.saveObject(dict)
         c = control.saveObject(dict)
@@ -133,48 +123,48 @@ class ShowAll(Screen):
     pass
 
 class Einzelansicht(FloatLayout):
-    #self.texte: dict[str, any] = {'None': None}
+    # self.texte: dict[str, any] = {'None': None}
     closeEinzelansicht = ObjectProperty(None)
-    entry_id = ObjectProperty()
-    entry_name = ObjectProperty()
-    entry_anzahl = ObjectProperty()
-    entry_kategorie = ObjectProperty()
-    entry_ausgeliehen = ObjectProperty()
-    entry_bemerkung = ObjectProperty()
-    entry_raum = ObjectProperty()
-    entry_typ = ObjectProperty()
-    entry_zustand = ObjectProperty()
+    entryId = ObjectProperty()
+    entryName = ObjectProperty()
+    entryAnzahl = ObjectProperty()
+    entryKategorie = ObjectProperty()
+    entryAusgeliehen = ObjectProperty()
+    entryBemerkung = ObjectProperty()
+    entryRaum = ObjectProperty()
+    entryTyp = ObjectProperty()
+    entryZustand = ObjectProperty()
 
     def entryFill(self):
         global mid
         self.texte = control.getObjectByID(int(mid))
-        self.entry_id.text = str(self.texte.get("ID", "None"))
-        self.entry_name.text = str(self.texte.get("name", "None"))
-        self.entry_raum.text = str(self.texte.get("raum", "None"))
-        self.entry_typ.text = str(self.texte.get("typ", "None"))
-        self.entry_kategorie.text = str(self.texte.get("kategorie", "None"))
-        self.entry_ausgeliehen.text = str(self.texte.get("ausgeliehen", "None"))
-        self.entry_zustand.text = str(self.texte.get("zustand", "None"))
-        self.entry_anzahl.text = str(self.texte.get("anzahl", "None"))
-        self.entry_bemerkung.text = str(self.texte.get("bemerkung", "None"))
+        self.entryId.text = str(self.texte.get("ID", "None"))
+        self.entryName.text = str(self.texte.get("name", "None"))
+        self.entryRaum.text = str(self.texte.get("raum", "None"))
+        self.entryTyp.text = str(self.texte.get("typ", "None"))
+        self.entryKategorie.text = str(self.texte.get("kategorie", "None"))
+        self.entryAusgeliehen.text = str(self.texte.get("ausgeliehen", "None"))
+        self.entryZustand.text = str(self.texte.get("zustand", "None"))
+        self.entryAnzahl.text = str(self.texte.get("anzahl", "None"))
+        self.entryBemerkung.text = str(self.texte.get("bemerkung", "None"))
 
     def speichern(self):
-        dataNew = {'ID': int(self.entry_id.text),
-                   'name': self.entry_name.text,
-                   'raum':self.entry_raum.text,
-                   'typ': self.entry_typ.text,
-                   'kategorie': self.entry_kategorie.text,
-                   'ausgeliehen': self.entry_ausgeliehen.text,
-                   'zustand': self.entry_zustand.text,
-                   'anzahl': self.entry_anzahl.text,
-                   'bemerkung':  self.entry_bemerkung.text}
+        dataNew = {'ID': int(self.entryId.text),
+                   'name': self.entryName.text,
+                   'raum': self.entryRaum.text,
+                   'typ': self.entryTyp.text,
+                   'kategorie': self.entryKategorie.text,
+                   'ausgeliehen': self.entryAusgeliehen.text,
+                   'zustand': self.entryZustand.text,
+                   'anzahl': self.entryAnzahl.text,
+                   'bemerkung': self.entryBemerkung.text}
         control.saveObject(dataNew)
 
     def kategorie(self):
         return control.getKategorie()
 
     def defektmelden(self):
-        #control.defektmelden(int(self.ids.entry_id.text))
+        # control.defektmelden(int(self.ids.entryId.text))
         pass
 
 
@@ -189,11 +179,11 @@ class AddUses(Screen):
 
 class TableBox(BoxLayout):
     def __init__(self, data, columns, **kwargs):
-        self.table_data = data
+        self.tableData = data
         self.columns = columns
         super().__init__(**kwargs)
         self.columns: int = 0
-        self.table_data: list = []
+        self.tableData: list = []
         self.popupWindow: Popup or None = None
 
 
@@ -201,84 +191,85 @@ class Table(BoxLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.columns: int = 0
-        self.table_data: list = []
+        self.tableData: list = []
         self.popupWindow: Popup or None = None
         self.popupEinzelansicht: Popup or None = None
         self.createTableData(control.getData())
 
     def createTableData(self, data: dict[str, dict[int, str]]):
-        column_titles = [x for x in data.keys()]
-        rows_length = len(data[column_titles[0]])
-        self.columns = len(column_titles)
+        columnTitles = [x for x in data.keys()]
+        rowsLength = len(data[columnTitles[0]])
+        self.columns = len(columnTitles)
 
-        self.table_data = []
+        self.tableData = []
 
-        for y in column_titles:
-            self.table_data.append(
+        for y in columnTitles:
+            self.tableData.append(
                 {'text': str(y), 'size_hint_y': None, 'height': 30, 'bcolor': (1.0, .31, 0.0, 1)})
-        for z in range(rows_length):
-            for y in column_titles:
-                self.table_data.append(
+        for z in range(rowsLength):
+            for y in columnTitles:
+                self.tableData.append(
                     {'text': str(data[y][z]), 'size_hint_y': None, 'height': 20, 'bcolor': (.06, .25, .50, 1)})
 
-    def callback_suche(self, text: str):
+    def callbackSuche(self, text: str):
         self.refreshTable(control.suche(text))
 
-    def refreshTable(self, table_data: dict[str, dict[int, str]]):
-        self.createTableData(table_data)
+    def refreshTable(self, tableData: dict[str, dict[int, str]]):
+        self.createTableData(tableData)
         self.createTable()
 
     def createTable(self):
         self.clear_widgets(self.children[:1])
-        self.add_widget(TableBox(self.table_data, self.columns))
+        self.add_widget(TableBox(self.tableData, self.columns))
 
-    def einzelansicht(self, id_input):
+    def einzelansicht(self, idInput):
         global mid
-        mid = id_input
+        mid = idInput
         show = Einzelansicht(closeEinzelansicht=self.closeEinzelansicht)
         self.popupEinzelansicht = Popup(title="Einzelansicht", title_align="center",
                                         content=show, auto_dismiss=True,
-                                        size_hint=(None,None), size=(600,400))
+                                        size_hint=(None, None), size=(600, 400))
         self.popupEinzelansicht.open()
         show.entryFill()
 
     def filter(self):
-        show = Popups(popup_close=self.popup_close, refreshTable=self.refreshTable)
+        show = Popups(popupClose=self.popupClose, refreshTable=self.refreshTable)
         self.popupWindow = Popup(title="Filter", title_align="center",
                                  content=show, auto_dismiss=True,
                                  size_hint=(None, None), size=(300, 400))
         self.popupWindow.open()
 
-    def add_GG(self):
-        show = PopupAddGG(popup_add_GG_close=self.popup_add_GG_close)
-        self.popupWindow_add_GG = Popup(title="Gebrauchsgegenstand hinzufügen", title_align="center",
-                                        content=show, auto_dismiss=True,
-                                        size_hint=(None, None), size=(300, 400))
-        self.popupWindow_add_GG.open()
+    def addGG(self):
+        show = PopupAddGG(popupAddGGClose=self.popupAddGGClose)
+        self.popupWindowAddGG = Popup(title="Gebrauchsgegenstand hinzufügen", title_align="center",
+                                      content=show, auto_dismiss=True,
+                                      size_hint=(None, None), size=(300, 400))
+        self.popupWindowAddGG.open()
 
-    def add_VG(self):
-        show = PopupAddVG(popup_add_VG_close=self.popup_add_VG_close)
-        self.popupWindow_add_VG = Popup(title="Verbrauchsgegenstand hinzufügen", title_align="center",
-                                        content=show, auto_dismiss=True,
-                                        size_hint=(None, None), size=(300, 400))
-        self.popupWindow_add_VG.open()
+    def addVG(self):
+        show = PopupAddVG(popupAddVGClose=self.popupAddVGClose)
+        self.popupWindowAddVG = Popup(title="Verbrauchsgegenstand hinzufügen", title_align="center",
+                                      content=show, auto_dismiss=True,
+                                      size_hint=(None, None), size=(300, 400))
+        self.popupWindowAddVG.open()
 
-    def popup_close(self):
+    def popupClose(self):
         self.popupWindow.dismiss()
 
-    def popup_add_GG_close(self):
-        self.popupWindow_add_GG.dismiss()
+    def popupAddGGClose(self):
+        self.popupWindowAddGG.dismiss()
 
-    def popup_add_VG_close(self):
-        self.popupWindow_add_VG.dismiss()
+    def popupAddVGClose(self):
+        self.popupWindowAddVG.dismiss()
 
     def closeEinzelansicht(self):
         self.popupEinzelansicht.dismiss()
         self.refreshTable(control.getData())
 
     def sortieren(self, spalte: str):
-        table=control.sortBy(spalte)
+        table = control.sortBy(spalte)
         self.refreshTable(table)
+
 
 class UIApp(App):
     """
@@ -286,7 +277,8 @@ class UIApp(App):
     """
 
     def build(self):
-        Window.size = (800, 600)
+        Window.size = (1200, 800)
+        Window.maximize()
         self.sManage = ScreenManager()
         self.sManage.add_widget(AddInv(name='addInv'))
         self.sManage.add_widget(AddConsum(name='addConsum'))
